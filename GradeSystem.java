6bbe6a19de5682b80fe6726395850b7c39049008
Porselvi86 <porselvielumalai86@gmail.com>
2026-05-28 21:27:09 +0530

    Else-if Ladder

---
+++ GradeSystem.java
@@ -0,0 +1,36 @@
+package Javaprograms;
+import java.util.Scanner;
+
+public class GradeSystem {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter marks:");
+		int marks = sc.nextInt();
+		
+		if(marks >= 90)
+		{
+			System.out.println("Grade A");
+		}
+		else if(marks >= 75)
+		{
+			System.out.println("Grade B");
+		}
+		else if(marks >= 55)
+		{
+			System.out.println("Grade C");
+		}
+		else if(marks >= 30)
+		{
+			System.out.println("Grade D");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+
+	}
+
+}
